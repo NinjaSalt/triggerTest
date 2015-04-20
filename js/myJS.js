2a2e2212914c6c57e2e1b472e8c2700c5d043188
NinjaSalt <jsalter@ucsc.edu>
2015-04-20 01:56:57 -0400

    First Commit

--- js/myJS.js
+++ js/myJS.js
@@ -1,29 +1,23 @@
-// invoke the carousel
 $('#myCarousel').carousel({
   interval: 3000
 });
-
-/* SLIDE ON CLICK */ 
-
-$('.carousel-linked-nav > li > a').click(function() {
-
-    // grab href, remove pound sign, convert to number
-    var item = Number($(this).attr('href').substring(1));
-
-    // slide to number -1 (account for zero indexing)
-    $('#myCarousel').carousel(item - 1);
-
-    // remove current active class
-    $('.carousel-linked-nav .active').removeClass('active');
-
-    // add active class to just clicked on item
-    $(this).parent().addClass('active');
-
-    // don't follow the link
-    return false;
+$('#myCarousel2').carousel({
+  interval: 5000
 });
 
-/* AUTOPLAY NAV HIGHLIGHT */
+//carousel navigation
+$('.carousel-linked-nav > li > a').click(function() {
+
+    var item = Number($(this).attr('href').substring(1));
+
+    $('#myCarousel').carousel(item - 1);
+
+    $('.carousel-linked-nav .active').removeClass('active');
+
+    $(this).parent().addClass('active');
+
+    return false;
+});
 
 // bind 'slid' function
 $('#myCarousel').bind('slid', function() {
@@ -39,18 +33,29 @@ $('#myCarousel').bind('slid', function() {
 
 });
 
+$('#myCarousel2').bind('slid', function() {
+
+
+    $('.carousel-linked-nav .active').removeClass('active');
+
+    // get index of currently active item
+    var idx = $('#myCarousel2 .item.active').index();
+
+    // select currently active item and add active class
+    $('.carousel-linked-nav li:eq(' + idx + ')').addClass('active');
+
+});
+
 $(document).ready(function() {
     
-    /* Every time the window is scrolled ... */
+    //Fade in different parts of the site
     $(window).scroll( function(){
     
-        /* Check the location of each desired element */
         $('.hideScroll').each( function(i){
             
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
-            /* If the object is completely visible in the window, fade it it */
             if( bottom_of_window > (bottom_of_object*.9) ){
                 
                 $(this).animate({'opacity':'1'},500);
